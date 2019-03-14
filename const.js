85ddbbbeba56ccc46dd5739d90dd5dcfb82ea204
onjuju <745512023@qq.com>
2019-03-14 12:14:50 +0800

    '添加环境设置'

--- const.js
+++ const.js
@@ -1,5 +1,17 @@
-const resUrl = 'http://192.168.0.102:8081'
-const mp3FilePath = 'D:/reborn/Reader/resource/mp3'
+const env = require('./env')
+
+let resUrl 
+let mp3FilePath
+if (env === 'dev') {
+  resUrl = 'http://192.168.0.102:8081'
+  mp3FilePath = 'D:/reborn/Reader/resource/mp3'
+} else {
+  // 生产环境
+  resUrl = 'http://47.112.108.171'
+  mp3FilePath = '/root/nginx/upload/mp3'
+}
+
+
 const category = [
   'Biomedicine',
   'BusinessandManagement',
